8370491db3bd9e21c8a122081c4470e5920ffca0
kchen2 <Kejun.Chen@nrel.gov>
2024-12-25 13:25:26 -0800

    Examples

--- examples/Convex_approximate_dynamic_programming.py
+++ examples/Convex_approximate_dynamic_programming.py
@@ -154,9 +154,9 @@ def main():
     variables = [P_sqrt, P_21, q]
     A_tch, B_tch, Q_tch, R_tch = map(torch.from_numpy, [A, B, Q, R])
 
-    variables_name = ["u"]
+    variables_name = [model.u]
     # variables_size = {'u':[m, 1], "y":[n, 1]}
-    parameters_name = ["q", "P21", "Psqrt", "x"]
+    parameters_name = [model.q, model.P21, model.Psqrt, model.x]
     # parameters_size = {'q':[m, 1], "P21" : [n, m], "Psqrt" : [m, m], "x": [n, 1]}
     model = model_instance()
     Pyomolayer = PyomoOptLayer(model, variables_name, parameters_name, solver = 'ipopt')
