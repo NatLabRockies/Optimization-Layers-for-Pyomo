f2913664ebeed4af565c636c8ab4faf12205ca6c
Chen, Kejun <Kejun.Chen@nrel.gov>
2025-01-09 09:46:20 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-
+# replace requirements.txt with requirements_laptop.txt when running on the laptop
 with open(os.path.join(here, "requirements.txt")) as f:
     install_requires = f.readlines()
 
